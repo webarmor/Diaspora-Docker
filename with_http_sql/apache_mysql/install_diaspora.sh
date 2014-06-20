6af6d0420badc3941b3414d89f6db20484e20bb4
Chocobozzz <djidane14ff@hotmail.fr>
2014-06-20 14:32:49 +0200

    First commit

---
+++ with_http_sql/apache_mysql/install_diaspora.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rvm use 2.0.0-p353
+cd diaspora
+
+RAILS_ENV=production bundle install --without test development
+RAILS_ENV=production bundle exec rake db:create db:schema:load
+bundle exec rake assets:precompile
---
+++ with_http_sql/apache_mysql/start.sh
@@ -0,0 +1,11 @@
+/usr/bin/mysqld_safe & 
+
+export APACHE_RUN_USER=www-data
+export APACHE_RUN_GROUP=www-data
+export APACHE_LOG_DIR=/var/log/apache2
+export APACHE_RUN_DIR=.
+/usr/sbin/apache2 -D BACKGROUND
+
+sleep 5
+
+sudo -u diaspora -i "/home/diaspora/diaspora/script/server"
