2c72fffe55aa5258d750637b28b16a7828c644dd
Chocobozzz <florian.chocobo@gmail.com>
2014-07-03 16:57:15 +0200

    Add nginx_mysql dev

---
+++ dev/nginx_mysql/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+mysqlRoot="mysqlroot"
+mysqlDiaspora=$(grep "^\s*[^#]\s*password" /home/diaspora/diaspora/config/database.yml | sed "s/\(\s*password:\s*\"\)//" | sed "s/\"\s*$//")
+
+# Start MySQL
+mysqld_safe &
+sleep 5
+
+# Create MySQL user
+mysqladmin -u root --password=temprootpass password $mysqlRoot
+echo "CREATE USER 'diaspora'@'localhost' IDENTIFIED BY '$mysqlDiaspora';" | \
+    mysql --user=root --password=$mysqlRoot
+echo "CREATE DATABASE IF NOT EXISTS diaspora_production DEFAULT CHARACTER SET \
+        'utf8' COLLATE 'utf8_unicode_ci';" | mysql --user=root --password=$mysqlRoot
+echo "GRANT SELECT, LOCK TABLES, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, \
+        ALTER ON diaspora_production.* TO 'diaspora'@'localhost';" | mysql \
+            --user=root --password=$mysqlRoot
+
+sudo -u diaspora -i /bin/bash --login /home/diaspora/install_diaspora.sh
---
+++ dev/nginx_mysql/start.sh
@@ -0,0 +1,9 @@
+/usr/bin/mysqld_safe & 
+
+/usr/bin/redis-server &
+
+/usr/sbin/nginx &
+
+sleep 5
+
+sudo -u diaspora -i "/home/diaspora/diaspora/script/server"
