21ccf0bdba680da1084418ee6af9654d4440fc76
root <root@mail.cpy.re>
2014-07-03 18:48:29 +0200

    Fix bug

--- dev/nginx_mysql/install.sh
+++ dev/nginx_mysql/install.sh
@@ -11,10 +11,17 @@ sleep 5
 mysqladmin -u root --password=temprootpass password $mysqlRoot
 echo "CREATE USER 'diaspora'@'localhost' IDENTIFIED BY '$mysqlDiaspora';" | \
     mysql --user=root --password=$mysqlRoot
-echo "CREATE DATABASE IF NOT EXISTS diaspora_production DEFAULT CHARACTER SET \
+echo "CREATE DATABASE IF NOT EXISTS diaspora_development DEFAULT CHARACTER SET \
         'utf8' COLLATE 'utf8_unicode_ci';" | mysql --user=root --password=$mysqlRoot
 echo "GRANT SELECT, LOCK TABLES, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, \
-        ALTER ON diaspora_production.* TO 'diaspora'@'localhost';" | mysql \
+        ALTER ON diaspora_development.* TO 'diaspora'@'localhost';" | mysql \
             --user=root --password=$mysqlRoot
+
+echo "CREATE DATABASE IF NOT EXISTS diaspora_test DEFAULT CHARACTER SET \
+        'utf8' COLLATE 'utf8_unicode_ci';" | mysql --user=root --password=$mysqlRoot
+echo "GRANT SELECT, LOCK TABLES, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, \
+        ALTER ON diaspora_test.* TO 'diaspora'@'localhost';" | mysql \
+            --user=root --password=$mysqlRoot
+
 
 sudo -u diaspora -i /bin/bash --login /home/diaspora/install_diaspora.sh
